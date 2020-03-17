d304612ba2035bbe947643b856987b8a69312adb
Viacheslav Orel <trosha2306@gmail.com>
2020-03-17 08:22:35 +0200

    20200317H

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+class Item {
+  purchase: string;
+  done: boolean;
+  price: number;
+
+  constructor(purchase: string, price: number) {
+    this.purchase = purchase;
+    this.price = price;
+    this.done = false;
+  }
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-purchase',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'purchaseapp';
+  text: string;
+  price = 0;
+
+  items: Item[] =
+    [
+      {purchase: 'Хлеб', done: false, price: 15.9},
+      {purchase: 'Масло', done: false, price: 60},
+      {purchase: 'Картофель', done: true, price: 22.6},
+      {purchase: 'Сыр', done: false, price: 310}
+    ];
+
+  addItem(text: string, price: number): void {
+
+    if (text == null || text.trim() === '' || price == null) {  return; }
+    this.items.push(new Item(text, price));
+  }
 }
